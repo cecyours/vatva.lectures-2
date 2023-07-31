9ad9a3b1860ec447691a897bfc45cf9dae371c3f
gkhell <gpkmyworld@gmail.com>
2023-07-31 17:39:42 +0530

    31/07/2023⠀⎦⎡⠀17:39⠀⎦⎢⠀Monday

---
+++ python-intern-jul/S01_P02_dataType.py
@@ -0,0 +1,27 @@
+# interpreter(python) / compiler(java/c++)
+# https://github.com/cecyours
+# numberic data
+# int , float, complex
+
+x = int(2.4)
+print(type(x))
+
+x = 10.4
+print(type(x))
+
+
+k = 10+4j
+j = 39+1j
+
+c = k+j
+print(c)
+
+
+# collection
+# string, list, tuple, dict, set
+
+# boolean
+# True False
+
+
+
