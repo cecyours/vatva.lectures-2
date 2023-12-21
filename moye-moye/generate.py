b7b414883933437542cd6aa86395972c222ac2fc
gkHell <gpkmyworld@gmail.com>
2023-12-21 21:12:40 +0530

    21/12/2023⠀⎦⎡⠀21:12⠀⎦⎢⠀Thursday

---
+++ moye-moye/generate.py
@@ -0,0 +1,13 @@
+
+import os
+import random
+import time
+n = 100
+while n:
+    n-=1
+    file = open("./data.txt","a+")
+    file.write("hello World")
+    file.close()
+    os.system("gdone")
+    time.sleep(1)
+
