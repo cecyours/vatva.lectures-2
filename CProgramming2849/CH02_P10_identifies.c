e188074b91a0529d255fe2ba1b5df92b04ea364c
gkHell <gpkmyworld@gmail.com>
2024-01-30 21:30:09 +0530

    30/01/2024⠀⎦⎡⠀21:30⠀⎦⎢⠀Tuesday

---
+++ CProgramming2849/CH02_P10_identifies.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/*
+
+1 . always starts with letter or under score
+2. it shoudn't starts with digit or special symbol except under score 
+3. no special symbols except underscore
+4. dublicate not allowed
+5. keywords are not used to .
+ */
+
+/**
+ * kite
+ * 
+ * sdf
+ * sd
+ * fds
+ * 
+ * dsf
+ * sd
+ * f
+*/
+int main()
+{
+
+    int _rollno; // valid
+    int rollno; //valid
+    // int 10rollno; // invalid
+    int _10rollno; // valid
+    
+    int rollno10; // valid
+    // int roll28374(*&#no10; //invalid
+    int a;
+    int a; // error
+
+    int float_;
+    return 0;
+}
---
+++ CProgramming2849/CH02_P11_dataType.c
@@ -0,0 +1,15 @@
+
+/**
+ * primitive / primary data type
+ * - int
+ * - float
+ * - char
+ * - signed
+ * 
+ * ---------
+ * derived data type
+ *  array ,string, pointer, function
+ * 
+ * user define 
+ * structure, union, enum
+*/
