ecfdf91eb7f7296bac84b80f7a927cba920febe4
gkHell <gpkmyworld@gmail.com>
2024-02-06 20:30:10 +0530

    06/02/2024⠀⎦⎡⠀20:30⠀⎦⎢⠀Tuesday

---
+++ CProgramming2849/CH02_P13_specifer.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main()
+{
+
+   unsigned short int i = 100;
+
+    printf("value  : %u\n", i);
+    printf("value  : %d\n", i);
+
+    return 0;
+}
---
+++ CProgramming2849/Swtich.js
@@ -0,0 +1,33 @@
+var number = 2-2+4
+
+switch (5) {
+  case 1:
+    console.log("number 1 Sunday ");
+    break;
+  case 2:
+    console.log("number 2 Monday ");
+    break;
+
+  case 3:
+    console.log("number 3 Tuesday");
+    break;
+
+  case 4:
+    console.log("number 4 Wednesday ");
+    break;
+
+  case 5:
+    console.log("number 5 thursday");
+    break;
+
+  case 6:
+    console.log("number 6 Friday");
+    break;
+
+  case 7:
+    console.log("number 7 saturday ");
+    break;
+
+    default:
+        console.log("Hu pote..")
+}
