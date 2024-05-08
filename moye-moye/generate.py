8a5a3d452f26968745aa0f9bea0e49df54ad3003
gkHell <gpkmyworld@gmail.com>
2024-05-08 15:16:27 +0530

    08/05/2024⠀⎦⎡⠀15:16⠀⎦⎢⠀Wednesday

--- moye-moye/generate.py
+++ moye-moye/generate.py
@@ -4,10 +4,11 @@ import random
 import time
 n = random.randint(500,720)
 while n:
+    d = random.randint(1,5)
     n-=1
     file = open("./data.txt","a+")
     file.write("hello World")
     file.close()
     os.system("gdone")
-    time.sleep(100)
+    time.sleep(d)
 
